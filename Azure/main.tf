ccd41bba957a68668834470d584ac78536f8a126
DiazGIT <cloud.dias@outlook.com>
2025-10-03 11:43:10 -0300

    Update main.tf

--- Azure/main.tf
+++ Azure/main.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
+backend "azurerm" {
+    resource_group_name  = "RG-REMOTE-STATE"
+    storage_account_name = "storemotestate01"
+    container_name       = "remote-state"
+    key                  = "lab-env/terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   # Configuration options
 }
+
