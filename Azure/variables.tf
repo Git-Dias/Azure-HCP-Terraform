d69e7b145550b06e2186e29edf48aae2977bb52d
DiazGIT <cloud.dias@outlook.com>
2025-10-03 11:48:32 -0300

    Update variables.tf

--- Azure/variables.tf
+++ Azure/variables.tf
@@ -1,1 +1,12 @@
+variable "rg_name" {
+  description = "Nome do Resource Group"
+  type        = string
+  default     = RG-HCP-Terraform"
+}
+
+variable "location" {
+  description = "Região do Azure"
+  type        = string
+  default     = "eastus"
+}
 
