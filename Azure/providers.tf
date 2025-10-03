895be1327c9fcd9404bc8c387b59687faeb53f7c
DiazGIT <cloud.dias@outlook.com>
2025-10-03 11:45:01 -0300

    Update providers.tf

--- Azure/providers.tf
+++ Azure/providers.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  backend "azurerm" {
+    resource_group_name   = "rg-terraform-state"
+    storage_account_name  = "stterraformstate001"
+    container_name        = "tfstate"
+    key                   = "lab.terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.70"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
 
