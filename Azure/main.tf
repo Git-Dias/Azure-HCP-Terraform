833913125521a5a19c03de8f1b50dc69a833055f
DiazGIT <cloud.dias@outlook.com>
2025-10-06 14:33:56 -0300

    Add comment

--- Azure/main.tf
+++ Azure/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_virtual_network" "lab_vnet" {
 
 resource "azurerm_virtual_network" "lab_vnet_02" {
   name                = "vnet-lab-02"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/16"] ##CIDR Block
   location            = azurerm_resource_group.hcp_tf_lab.location
   resource_group_name = azurerm_resource_group.hcp_tf_lab.name
 }
